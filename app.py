962e731d7e24545250ad516c127e861a9cbab5b8
elvysribeiro <elvyssribeiro@gmail.com>
2021-12-14 14:52:54 -0300

    Contato estilizado, email do py configurado.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, request, flash
 from flask_mail import Mail, Message
-from dotenv import load_dotenv
-import os
-load_dotenv()
+from config import email,senha
+
+
 
 app = Flask(__name__)
 app.secret_key = 'devweb'
@@ -12,8 +12,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.getenv("EMAIL"),
-    "MAIL_PASSWORD": os.getenv("SENHA")
+    "MAIL_USERNAME": email,
+    "MAIL_PASSWORD": senha
 }
 
 app.config.update(mail_settings)
@@ -21,8 +21,8 @@ mail = Mail(app)
 
 class Contato:
     def __init__(self, nome, email, mensagem):
-        self.nome = nome
-        self.email = email
+        self.nome = nome,
+        self.email = email,
         self.mensagem = mensagem
 
 @app.route('/')
