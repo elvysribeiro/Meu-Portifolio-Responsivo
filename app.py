3b30869cb966cabcfb552cbe9b80813f3d6726a9
elvysribeiro <elvyssribeiro@gmail.com>
2021-12-14 10:38:13 -0300

    atuallizações no contato

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from Flask import Flask, render_template,redirect
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-@pp.route('/')
+@app.route('/')
 def index():
     return render_template('index.html') 
 
